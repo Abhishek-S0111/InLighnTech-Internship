e0f470c2e29e50b7e60f7faab6a5d82c6b9daa28
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-08-01 11:09:07 +0530

    Updated streamlit app

--- streamlit/App.py
+++ streamlit/App.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import joblib
 import pandas as pd
+import time
 
 @st.cache_resource
 def load_model():
@@ -14,16 +15,23 @@ st.title("Real-Time Fraud Detection Simulator")
 
 fraud, legit = 0,0
 
+placeholder = st.empty()
+
 if st.button("Start Simulation"):
     for i in range(len(X)):
         row = X.iloc[i:i+1]
         pred = model.predict(row)[0]
         proba = model.predict_proba(row)[0][1]
         
-        if(pred == 1):
+        if pred == 1:
             fraud += 1
-            st.write(f"Transaction #{i+1}: {'FRAUDULENT' if pred else 'LEGITIMATE'} | Confidence: {proba:.2f}")
+            placeholder.write(f"Transaction #{i+1}: FRAUDULENT | Confidence: {proba:.2f}")
         else:
             legit += 1
+            placeholder.write(f"Transaction #{i+1}: LEGITIMATE | Confidence: {proba:.2f}")
         
-    st.write(f"===================Final Results================\nLegitimate Transactions : {legit}\nFraudulent Transactions : {fraud}")
+        time.sleep(0.1)  # simulate delay
+
+    st.write("---")
+    st.write(f"✅ Legitimate Transactions: {legit}")
+    st.write(f"⚠️ Fraudulent Transactions: {fraud}")
